d8ee4fef169b0db356011a3cedea11829d685a22
0124hitesh <hitesh.sharma_cs18@gla.ac.in>
2021-10-28 11:44:52 +0530

    promises & API( XHR, fetch)

---
+++ promise.js
@@ -0,0 +1,31 @@
+console.log('start')
+
+// const p = new Promise(function(resolve, reject){
+//     const ran = Math.random();
+//     console.log('Random = ' + ran)
+//     if(ran > .5){
+//         resolve()
+//     }
+//     else{
+//         reject()
+//     }
+// }).then(function(data){
+//     console.log('Resolved ', data)
+// }, function(d){
+//     console.log('Rejected ', data)
+// })
+
+const p2 = new Promise(function(){
+    const ran = Math.random();
+    console.log('Random = ' + ran)
+    if(ran > .5){
+        resolve()
+    }
+    else{
+        reject()
+    }
+}).then(()=> console.log("Resolved")).catch(()=> console.log("Rejected"))
+
+console.log(p2)
+// console.log(p)
+console.log('End')
